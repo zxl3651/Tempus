1aa7dfeb426c6f5d1a12bbad62410f85a3174219
S_HYEON <zxl3651@naver.com>
2024-09-10 23:44:40 +0900

    🎉 [Init] Tempus 프로젝트 초기 생성

---
+++ Tempus/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tempus
+//
+//  Created by 이성현 on 2024/09/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Tempus/TempusApp.swift
@@ -0,0 +1,17 @@
+//
+//  TempusApp.swift
+//  Tempus
+//
+//  Created by 이성현 on 2024/09/10.
+//
+
+import SwiftUI
+
+@main
+struct TempusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
